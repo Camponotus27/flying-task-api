490d85cbf99333a2ee8c64c7675fdcc3fe395bb7
Luis Jara <lupredatortech1@gmail.com>
2021-08-16 21:28:00 -0400

    Initial Commit

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Task;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Task::insert([
+
+            [
+                'id' => 1,
+                'title' => "labeiales",
+                'body' => 'hola',
+                'updated_at' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),                 
+            ],
+            [
+                'id' => 2,
+                'title' => 'bases',
+                'body' => 'los pajaros alguna ves volaron',
+                'updated_at' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
+            ],
+             [
+                'id' => 3,
+                'title' => 'rubores',
+                'body' => ':)',
+                'updated_at' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
+            ],
+             [
+                'id' => 4,
+                'title' => 'sombras',
+                'body' => 'loading',
+                'updated_at' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
+            ],
+             [
+                'id' => 5,
+                'title' => 'bronsers',
+                'body' => ':(',
+                'updated_at' => date('Y-m-d H:i:s'),
+                'created_at' => date('Y-m-d H:i:s'),
+            ],
+      ]);
+    }
+}
