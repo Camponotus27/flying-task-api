19b1a94767ceffce93947ffe44063f5a7d637887
Sebastian <sebastian.cornejo@walmart.com>
2021-08-30 10:35:08 -0400

    up max rules validation leng body int Task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,7 +14,7 @@ class TaskController extends ApiController
     protected $model = 'App\Models\Task';
     protected $rules_validation = [
         'title' => 'required|string|max:255',
-        'body' => 'required|string|max:1000'
+        'body' => 'required|string|max:60000'
     ];
 
     public function __construct()
