3cee1e63cc826543ea2988aa6c7b062e95655c76
Sebastian <sebastian.cornejo@walmart.com>
2021-10-24 10:18:38 -0300

    add scopeindex orderby updated_at

--- app/Models/EnglishDictionary.php
+++ app/Models/EnglishDictionary.php
@@ -5,4 +5,8 @@ namespace App\Models;
 class EnglishDictionary extends GenericModel
 {
     protected $fillable = ['word','pronunciation', 'significance', 'note'];
+    public function scopeIndex($query)
+    {
+        return $query->orderByDesc('updated_at');
+    }
 }
